08ad971b00278481aab0ccf804e781344f901f4e
OxzaLic <phagamas.khiew@vru.ac.th>
2024-12-08 13:04:42 +0700

    Initial commit

---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>QUIZ1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Gallery of Phagamas 66222420028</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg">
+                <h2> ant </h2>
+                <a href="gallery/ant"><img src={{$ant}} alt="ant" style="width:100px;height:100px;"></a>
+            </div>
+            <div class="col-lg">
+                <h2> bird </h2>
+                <a href="gallery/bird"> <img src={{$bird}} alt="birda" style="width:100px;height:100px;"></a>
+            </div>
+            <div class="col-lg">
+                <h2> cat </h2>
+                <a href="gallery/cat"><img src={{$cat}} alt="cat" style="width:100px;height:100px;"></a>
+            </div>
+            <div class="col-lg">
+                <h2> god </h2>
+                <a href="https://www.w3schools.com/"> <img src={{$god}} alt="god" style="width:100px;height:100px;"></a>
+            </div>
+            <div class="col-lg">
+                <h2> spider </h2>
+                <a href="https://www.w3schools.com/"> <img src={{$spider}} alt="spider" style="width:100px;height:100px;"></a>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
